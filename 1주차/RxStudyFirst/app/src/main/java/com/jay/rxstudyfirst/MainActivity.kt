8f9737c8b0bd15574df0711bdf1faec629aad8f9
wj1227 <wj122791@gmail.com>
2020-09-01 00:38:21 +0900

    single 성공?

--- 1주차/RxStudyFirst/app/src/main/java/com/jay/rxstudyfirst/MainActivity.kt
+++ 1주차/RxStudyFirst/app/src/main/java/com/jay/rxstudyfirst/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.constraintlayout.utils.widget.MockView
 import androidx.recyclerview.widget.RecyclerView
 import io.reactivex.Observable
 import io.reactivex.Scheduler
+import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
 import io.reactivex.rxkotlin.addTo
@@ -25,7 +26,9 @@ class MainActivity : AppCompatActivity() {
 
     private val compositeDisposable = CompositeDisposable()
     private lateinit var recyclerView: RecyclerView
-    private lateinit var btnApiCall: Button
+    private lateinit var btnSingle: Button
+    private lateinit var btnMaybe: Button
+    private lateinit var btnCompletable: Button
     private lateinit var adapter: MainAdapter
     private lateinit var progressbar: ProgressBar
 
@@ -44,7 +47,9 @@ class MainActivity : AppCompatActivity() {
 
     private fun initView() {
         recyclerView = findViewById(R.id.recycler_view)
-        btnApiCall = findViewById(R.id.btn_call)
+        btnSingle = findViewById(R.id.btn_single)
+        btnMaybe = findViewById(R.id.btn_maybe)
+        btnCompletable = findViewById(R.id.btn_completable)
         progressbar = findViewById(R.id.progress_bar)
     }
 
@@ -57,18 +62,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun initClickListener() {
-        btnApiCall.setOnClickListener {
-            asdf(api.getMovies())
-            api.getMovies().test()
-//            api.getMovies().subscribeOn(Schedulers.io())
-//                .observeOn(AndroidSchedulers.mainThread())
-//                .doOnSubscribe { isLoading(true) }
-//                .doAfterTerminate { isLoading(false) }
-//                .subscribe({
-//                    Log.d(TAG, "success: $it")
-//                }, {
-//                    Log.d(TAG, "fail: ${it.message}")
-//                }).addTo(compositeDisposable)
+        btnSingle.setOnClickListener {
+            api.getMovies().rxSingle()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { isLoading(true) }
+                .doAfterTerminate { isLoading(false) }
+                .subscribe({
+                    Log.d(TAG, "ok: $it")
+                }, {
+                    Log.d(TAG, "initClickListener: ${it.message}")
+                }).addTo(compositeDisposable)
+        }
+
+        btnMaybe.setOnClickListener {
+
+        }
+
+        btnCompletable.setOnClickListener {
+
         }
     }
 
@@ -80,55 +92,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    fun <T> test(ooo: Call<T>) : Observable<T> {
-        ooo.enqueue(object : Callback<T> {
-            override fun onFailure(call: Call<T>, t: Throwable) {
-                Log.d(TAG, "onFailure: ${t.message}")
-            }
+    fun <T> Call<T>.rxSingle(): Single<T> {
+        return Single.create { emitter ->
+            this.enqueue(object : Callback<T> {
+                override fun onFailure(call: Call<T>, t: Throwable) {
+                    Log.d(TAG, "onFailure: ${t.message}")
+                }
 
-            override fun onResponse(call: Call<T>, response: Response<T>) {
-                Log.d(TAG, "onResponse: $response")
-            }
-        })
-
-    }
-
-//    fun <T> test (call: Call<MovieResponse>, c: Observable<MovieResponse>): Observable<T> {
-//        call.enqueue(object : Callback<MovieResponse> {
-//            override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-//                TODO("Not yet implemented")
-//            }
-//
-//            override fun onResponse(call: Call<MovieResponse>, response: Response<MovieResponse>) {
-//                TODO("Not yet implemented")
-//            }
-//        })
-//
-//    }
-    fun <T> asdf(ccc: Observable<Call<T>>): T {
-            ccc.subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread())
-                .subscribe({
-                    it.enqueue(object : Callback<MovieResponse> {
-                        override fun onFailure(call: Call<MovieResponse>, t: Throwable) {
-                            Log.d(TAG, "onFailure: ${t.message}")
-                        }
-
-                        override fun onResponse(
-                            call: Call<MovieResponse>,
-                            response: Response<MovieResponse>
-                        ) {
-                            val body = response.body()
-                            if (response.isSuccessful && body != null) {
-                                Log.d(TAG, "onResponse: $body")
-                            } else {
-                                Log.d(TAG, "onResponse: else")
-                            }
-                        }
-                    })
-                }, {
-                    Log.d(TAG, "asdf: ${it.message}")
-                }).addTo(compositeDisposable)
+                override fun onResponse(call: Call<T>, response: Response<T>) {
+                    val body = response.body()
+                    if (response.isSuccessful && body != null) {
+                        Log.d(TAG, "onResponse: $body")
+                        emitter.onSuccess(body)
+                    } else {
+                        Log.d(TAG, "onResponse else: $response")
+                        emitter.onError(Throwable("error"))
+                    }
+                }
+            })
+        }
     }
 
     override fun onDestroy() {
